6c3067ab52a5a72be0d6f1c0de0dd58a2a6510ae
jhtong <me@joeltong.org>
2019-07-26 15:08:52 +0800

    removed debugging code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import util from 'util';
 import { scrapeOffers } from './lib/fetcher';
 
-(async () => {
-  const results = await scrapeOffers({ page: 25 });
-  //   console.dir(results);
-  console.log(util.inspect(results, false, null, true /* enable colors */));
-  console.log(results.length);
-})();
+// (async () => {
+//   const results = await scrapeOffers({ page: 25 });
+//   //   console.dir(results);
+//   console.log(util.inspect(results, false, null, true /* enable colors */));
+//   console.log(results.length);
+// })();
 
 const ChopeScraper = { scrapeOffers };
 
